7c4ef1d1a7c3df60c6133500576975956b796485
Curzeed <xavier.lecapitaine4450@gmail.com>
2022-11-07 01:09:45 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -2,10 +2,69 @@ import './App.css';
 import CardOverviewComponent from './components/CardOverviewComponent';
 import CardSocials from './components/CardSocialsComponent';
 import Container from './components/ContainerComponent';
-import NavBar from './components/NavbarComponent';
+import NavBar from './components/NavbarComponent';  
 import SubNavBar from './components/SubNavBarComponent';
-const arr = Array(9)
+
+
+
 function App() {
+  const data = [{
+    title : "Page Views",
+    number : 87,
+    social : "facebook",
+    benefits : "3%",
+    status : "up"
+  },
+  {
+    title : "Likes",
+    number : 52,
+    social : "facebook",
+    benefits : "3%",
+    status : "down"
+  },
+  {
+    title : "Likes",
+    number : 5462,
+    social : "instagram",
+    benefits : "2257%",
+    status : "up"
+  },
+  {
+    title : "Profile views",
+    number : "52k",
+    social : "instagram",
+    benefits : "1375%",
+    status : "up"
+  },
+  {
+    title : "Retweets",
+    number : 117,
+    social : "twitter",
+    benefits : "303%",
+    status : "up"
+  },
+  {
+    title : "Likes",
+    number : 507,
+    social : "twitter",
+    benefits : "553%",
+    status : "up"
+  },
+  {
+    title : "Likes",
+    number : 107,
+    social : "youtube",
+    benefits : "19%",
+    status : "down"
+  },
+  {
+    title : "Total views",
+    number : 1407,
+    social : "youtube",
+    benefits : "12%",
+    status : "down"
+  }
+]
   return (
     <div className="App">
       < NavBar title="Social Media Dashboard"/>
@@ -17,14 +76,12 @@ function App() {
       </Container>
       <SubNavBar title="Overview - Today"/>
       <Container type="-cards-overview">
-        <CardOverviewComponent title="Page Views" number="87" social="facebook" benefits="3%" status="up"/>
-        <CardOverviewComponent title="Likes" number="52" social="facebook" benefits="2%" status="down"/>
-        <CardOverviewComponent title="Likes" number="5462" social="instagram" benefits="2257%" status="up"/>
-        <CardOverviewComponent title="Profile views" number="52k" social="instagram" benefits="1375%" status="up"/>
-        <CardOverviewComponent title="Retweets" number="117" social="twitter" benefits="303%" status="up"/>
-        <CardOverviewComponent title="Likes" number="507" social="twitter" benefits="553%" status="up"/>
-        <CardOverviewComponent title="Likes" number="107" social="youtube" benefits="19%" status="down"/>
-        <CardOverviewComponent title="Total views" number="1407" social="youtube" benefits="12%" status="down"/>
+      {
+        data.map((item, index) => {
+          return (<CardOverviewComponent title={item.title} number={item.number} 
+            social={item.social} benefits={item.benefits} status={item.status}/>
+        )})
+      }
       </Container>
     </div>
   );
